8d25d6c4d5f2da0c3ca07a029408c7d9d4d599aa
darabsp <51314762+darabsp@users.noreply.github.com>
2021-12-26 03:25:09 +0900

    [fix]動いたので<br>追加、小数点以下2桁目で四捨五入

--- main.js
+++ main.js
@@ -120,8 +120,8 @@ async function loop() {
     };
     let bestClass = prediction[bestPredictionIndex].className, bestProbability = prediction[bestPredictionIndex].probability * 100;
     let firstSentence = "このお菓子は「" + bestClass + "」でしょう。";
-    let secondSentence = "確信率は" + bestProbability.toFixed(3) + "%です。"
-    document.getElementById("answer-container").textContent = firstSentence + secondSentence;
+    let secondSentence = "確信率は" + bestProbability.toFixed(1) + "%です。"
+    document.getElementById("answer-container").textContent = firstSentence + "<br>" + secondSentence;
     window.requestAnimationFrame(loop);
 }
 
